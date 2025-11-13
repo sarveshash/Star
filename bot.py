760e6ccc57e9959cc67c798778b3ba69af0b872e
sarveshash <136432405+sarveshash@users.noreply.github.com>
2025-11-13 19:17:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ API_ID = 27715449
 API_HASH = "dd3da7c5045f7679ff1f0ed0c82404e0"
 BOT_TOKEN = "8397651199:AAGPUiPNlr4AkgGoQK6BWAeyK4uCYL0knJ4"
 
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+bot = TelegramClient('app', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
 VIDEO_URL = "https://files.catbox.moe/491yle.mp4"
 
